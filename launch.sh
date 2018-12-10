7769688a4cb0a45c96aa11c1af951b38e34dcfaa
mipere08 <mipere08@ucm.es>
2018-12-10 21:00:30 +0100

    added launch script for java service working

--- launch.sh
+++ launch.sh
@@ -7,81 +7,64 @@ PATH_TO_JAR=/home/user-iot/gprc-practise/target/coffeeservice-1.0.0-SNAPSHOT-jar
 PID_PATH_NAME=/tmp/MyService-pid
 
 
+
+#  nohup java -jar /home/user-iot/gprc-practise/target/coffeeservice-1.0.0-SNAPSHOT-jar-with-dependencies.jar /tmp 2>> /dev/null >> /dev/null &
+
 case $1 in
+ #    start)
+ #        echo "1 Starting $SERVICE_NAME ..."
+ #            nohup java -jar $PATH_TO_JAR 2>> /dev/null >> /dev/null &
+ #                        echo $! > $PID_PATH_NAME
+ #            echo "2 $SERVICE_NAME started ..."
+ #    ;;
+ #    stop)
+ #            PID=$(cat $PID_PATH_NAME);
+ #            echo "0 STOP $PID PID .............."
+ #            echo "4 $SERVICE_NAME stoping ..."
+ #            kill $PID;
+ #            echo "5 $SERVICE_NAME stopped ..."
+ #            rm $PID_PATH_NAME
+	# ;;
+
     start)
-        echo "Starting $SERVICE_NAME ..."
+        echo "1 Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+            nohup java -jar $PATH_TO_JAR 2>> /dev/null >> /dev/null &
                         echo $! > $PID_PATH_NAME
-            echo "$SERVICE_NAME started ..."
+            echo "2 $SERVICE_NAME started ..."
         else
-            echo "$SERVICE_NAME is already running ..."
+            echo "3 $SERVICE_NAME is already running ..."
         fi
     ;;
     stop)
         if [ -f $PID_PATH_NAME ]; then
             PID=$(cat $PID_PATH_NAME);
-            echo "$SERVICE_NAME stoping ..."
+            echo "0 STOP $PID PID .............."
+            echo "4 $SERVICE_NAME stoping ..."
             kill $PID;
-            echo "$SERVICE_NAME stopped ..."
+            echo "5 $SERVICE_NAME stopped ..."
             rm $PID_PATH_NAME
         else
-            echo "$SERVICE_NAME is not running ..."
+            echo "6 $SERVICE_NAME is not running ..."
         fi
     ;;
+
+
     restart)
         if [ -f $PID_PATH_NAME ]; then
             PID=$(cat $PID_PATH_NAME);
-            echo "$SERVICE_NAME stopping ...";
+            echo "0 RESTART $PID PID .............."
+            echo "7 $SERVICE_NAME stopping ...";
             kill $PID;
-            echo "$SERVICE_NAME stopped ...";
+            echo "8 $SERVICE_NAME stopped ...";
             rm $PID_PATH_NAME
-            echo "$SERVICE_NAME starting ..."
-            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+            echo "9 $SERVICE_NAME starting ..."
+            nohup java -jar $PATH_TO_JAR 2>> /dev/null >> /dev/null &
                         echo $! > $PID_PATH_NAME
-            echo "$SERVICE_NAME started ..."
+            echo "10 $SERVICE_NAME started ..."
         else
-            echo "$SERVICE_NAME is not running ..."
+            echo "11 $SERVICE_NAME is not running ..."
         fi
     ;;
 esac
 
-
-# case $1 in
-# 	start)
-# 		echo "Starting  $SERVICE_NAME  ..."
-# 		if [ ! -f $PID_PATH_NAME  ]; then
-# 			nohup  java -jar  $PATH_TO_JAR /tmp 2>> /dev/null  >> /dev/null &
-# 			echo $! > $PID_PATH_NAME
-# 			echo "$SERVICE_NAME  started  ..."
-# 		else
-# 			echo "$SERVICE_NAME  is  already  running  ..."
-# 		fi
-# 	;;
-	# stop)
-	# 	if [ -f $PID_PATH_NAME  ]; then
-	# 		PID=$(cat  $PID_PATH_NAME);
-	# 		echo "$SERVICE_NAME  stoping  ..."
-	# 		kill  $PID;
-	# 		echo "$SERVICE_NAME  stopped  ..."
-	# 		rm  $PID_PATH_NAME
-	# 	else
-	# 		echo "$SERVICE_NAME  is not  running  ..."
-	# 	fi
-	# ;;
-	# restart)
-	# 	if [ -f $PID_PATH_NAME  ]; then
-	# 		PID=$(cat  $PID_PATH_NAME);
-	# 		echo "$SERVICE_NAME  stopping  ...";
-	# 		kill  $PID;
-	# 		echo "$SERVICE_NAME  stopped  ...";
-	# 		rm  $PID_PATH_NAME
-	# 		echo "$SERVICE_NAME  starting  ..."
-	# 		nohup  java -jar  $PATH_TO_JAR /tmp 2>> /dev/null  >> /dev/null &
-	# 			echo $! > $PID_PATH_NAME
-	# 		echo "$SERVICE_NAME  started  ..."
-	# 	else
-	# 		echo "$SERVICE_NAME  is not  running  ..."
-	# 	fi
-	# ;;
-# esac
